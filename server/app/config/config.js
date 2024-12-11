07afd0cb640137dd78c333ae76ec71ca42c7c530
MihajloVelickovic <mvelickovic03@gmail.com>
2024-12-11 08:22:25 +0100

    U nekoj od verzija noda npm kad su dodali native --watch parametar pa ne mora nodemon, i native --env-file pa ne mora env file loader fajl uopste i ne treba dotenv package

--- server/app/config/config.js
+++
@@ -1,8 +0,0 @@
-import dotnev from "dotenv";
-import path from "path";
-
-const env_file_path = process.platform.startsWith("win") ? 
-                      path.resolve(__dirname, "../../../config.env") :
-                      path.resolve(__dirname, "../../../.env");
-
-dotnev.config({path: env_file_path});
