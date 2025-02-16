b76bbdd09d82c559f2b9721cdfd3907e32943a1d
KingLaza <lazarbecic1531@gmail.com>
2025-02-16 02:15:54 +0100

    Configured Redis with Redis Client, as well as testing it

---
+++ server/app/config/config.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Create __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const envFilePath = process.platform.startsWith("win") ?
+                    path.join(__dirname, "../config.env") : 
+                    path.join(__dirname, "../.env");
+
+console.log("file path: " + envFilePath);
+
+dotenv.config({ path: envFilePath });
+
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+export const REDIS_HOST = process.env.REDIS_HOST;
+export const REDIS_PORT = process.env.REDIS_PORT;
