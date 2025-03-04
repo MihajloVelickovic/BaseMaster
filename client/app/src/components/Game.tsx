281e53a6c753e1fc1c4fb0787c789de73c52ff4d
KingLaza <lazarbecic1531@gmail.com>
2025-03-04 23:57:59 +0100

    Added number generator and result checker, as well as some appearance changes

--- client/app/src/components/Game.tsx
+++ client/app/src/components/Game.tsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import "../styles/Game.css";
 import { useLocation } from "react-router-dom";
 
+const maxVal:bigint = BigInt(255);
+var numToFind = getRandomNumber(1, Number(maxVal));
 
 function clcBtnCount(base:bigint, maxValue:bigint) {
   var count = 0, currVal=maxValue;
@@ -12,6 +14,12 @@ function clcBtnCount(base:bigint, maxValue:bigint) {
   return count;
 }
 
+function getRandomNumber(min:number, max:number) {
+  min = Math.floor(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function Game() {
 
   const location = useLocation();
@@ -19,7 +27,7 @@ function Game() {
 
   console.log("base: "+base+" playerNum: "+playerNum+" gameMode: "+gameMode);
 
-  const maxVal:bigint = BigInt(255);
+  
   const numOfButtons = clcBtnCount(BigInt(base), maxVal);
   const [arrayOfValues, setArrayOfValues] = useState(Array.from({length: numOfButtons}, (_, i) => 0));
   const btnArrayLabels = Array.from({ length: numOfButtons}, (_, i) => Math.pow(Number(base), numOfButtons - 1 - i));
@@ -43,7 +51,14 @@ function Game() {
   }
 
   function confirmButtonHandler() {
-    alert("Idk bro... Maybe it's right, maybe it's not.. That's like moral.. what is good, what is bad? Who knows?!");
+    //alert("Idk bro... Maybe it's right, maybe it's not.. That's like moral.. what is good, what is bad? Who knows?!");
+    const result = arrayOfValues.map((val, index) => val * btnArrayLabels[index]).reduce((sum, val) => sum + val, 0);
+    if (result==numToFind){
+      alert("You got the right answer!");
+      numToFind = getRandomNumber(1, Number(maxVal));
+      clearButtonHandler();
+    }else
+      alert("Better luck next time");
   }
 
   function generateBaseButtons(btnCount: number) {
@@ -69,7 +84,7 @@ function Game() {
   return (
     <div className="Game">
       <div>
-        <label>I guess the target number should be here </label>
+        <label className="NumToFindLabel">({numToFind})<label className="smallNumToFindLabel">10</label> = (?)<label className="smallNumToFindLabel">{base}</label> </label>
         {generateBaseButtons(numOfButtons)}
       </div>
       <button className= "ClearButton" onClick={clearButtonHandler}>
