1671e919a010201bda8d28d66616938fc3e8eb0d
MihajloVelickovic <mvelickovic03@gmail.com>
2025-03-12 00:44:06 +0100

    sklonjeni gitkeepovi

--- server/app/models/gameInfo.ts
+++ server/app/models/gameInfo.ts
@@ -3,7 +3,6 @@ export default class GameInfo {
     randomNumbers:number[];
     gameId:string;
     
-
     constructor(randomNumbers, gameId) {
        this.randomNumbers = randomNumbers;
        this.gameId = gameId;
