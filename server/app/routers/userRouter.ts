0deb15006376b7bb27366be51bc5d656c0f4b836
MihajloVelickovic <mvelickovic03@gmail.com>
2025-04-13 23:03:50 +0200

    Log in korisnika Neo4J, i za ovo i za registraciju treba dodati JWT todo todo

--- server/app/routers/userRouter.ts
+++ server/app/routers/userRouter.ts
@@ -20,99 +20,104 @@ userRouter.post("/register", async(req: any, res: any) => {
     if(password !== confirmPassword)
         return res.status(400).json({message: "Passwords do not match"});
 
-    const n4jSesh = n4jSession();
+    try{
+        const n4jSesh = n4jSession();
 
-    let query = await n4jSesh.executeRead(transaction => {
-        const result = transaction.run(`MATCH(n:User{email: $email}) 
-                               RETURN n IS NOT NULL AS alreadyExists`, {email})
-                         .then(result => {
-            const resultBody = result.records[0]?.get("alreadyExists") ?? false;
-            return resultBody;
+        let query = await n4jSesh.executeRead(transaction => {
+            const result = transaction.run(`MATCH(n:User{email: $email}) 
+                                RETURN n IS NOT NULL AS alreadyExists`, {email})
+                            .then(result => {
+                const resultBody = result.records[0]?.get("alreadyExists") ?? false;
+                return resultBody;
+            });
+            return result;
         });
-        return result;
-    });
 
-    if(query)
-        return res.status(400).json({message: "User with this email already exists"});
+        if(query)
+            return res.status(400).json({message: "User with this email already exists"});
 
-    query = await n4jSesh.executeRead(transaction => {
-        const result = transaction.run(`MATCH(n:User{username: $username}) 
-                                        RETURN n IS NOT NULL AS alreadyExists`, 
-                                        {username})
-                                  .then(result => {
-                        const resultBody = result.records[0]?.get("alreadyExists") ?? false;
-                        return resultBody;
-                    });
-        return result;
-    });
+        query = await n4jSesh.executeRead(transaction => {
+            const result = transaction.run(`MATCH(n:User{username: $username}) 
+                                            RETURN n IS NOT NULL AS alreadyExists`, 
+                                            {username})
+                                    .then(result => {
+                            const resultBody = result.records[0]?.get("alreadyExists") ?? false;
+                            return resultBody;
+                        });
+            return result;
+        });
 
-    if(query)
-        return res.status(400).json({message: "Username already taken"});
+        if(query)
+            return res.status(400).json({message: "Username already taken"});
 
-    const register = await n4jSesh.executeWrite(transaction => {
+        const register = await n4jSesh.executeWrite(transaction => {
 
-        const result = transaction.run(`CREATE(n:User{email: $email, username: $username, password: $password})
-                                        RETURN true AS success`, 
-                                        {username, email, password})
-                                  .then(result => {
-                        const resultBody = result.records[0]?.get("success") ?? false;
-                        return resultBody;})
-                                  .catch(err => err);
-        return result;
-    });
+            const result = transaction.run(`CREATE(n:User{email: $email, username: $username, password: $password})
+                                            RETURN true AS success`, 
+                                            {username, email, password})
+                                    .then(result => {
+                            const resultBody = result.records[0]?.get("success") ?? false;
+                            return resultBody;})
+                                    .catch(err => err);
+            return result;
+        });
 
-    return register ?
-    res.status(200).json({message: `User '${username}' successfully created!`}):
-    res.status(400).json({message: `Failed to create user "${username}"\n${register}`});
+
+        //TODO email potvrda
+
+        n4jSesh.close();
+
+        return register ?
+        res.status(200).json({message: `User '${username}' successfully created!`}):
+        res.status(400).json({message: `Failed to create user "${username}"\n${register}`});
+    }
+    catch(error){
+        return res.status(500).json({message:"How did this happen....", error: error});
+    }
+    
+        
+});
+
+
+userRouter.post("/login", async(req: any, res: any) => {
+
+    console.log("someone is signing in 🕺🕺🕺");
+
+    const {emailOrUsername, password} = req.body;
+
+    try{
+        const n4jSesh = n4jSession();
+
+        let user = await n4jSesh.executeRead(transaction => {
+            const result = transaction.run(`MATCH(n:User)
+                                            WHERE n.username = $emailOrUsername OR n.email = $emailOrUsername
+                                            RETURN {username: n.username, password: n.password}as user`, {emailOrUsername})
+                                    .then(result => {
+                            const resultBody = result.records[0]?.get("user") ?? false;
+                            return resultBody;
+            });
+            return result;
+        });
+
+        if(!user)
+            return res.status(400).json({message: `User '${emailOrUsername}' does not exist`});
+
+        if(password != user.password)
+            return res.status(400).json({message: "Incorrect password"});
+
+        //TODO JWT
+        
+        n4jSesh.close();
+
+        return res.status(200).json({message: "Success"});
+    }
+    catch(error){
+        return res.status(500).json({message:"How did this happen....", error: error});
+    }
 
 });
 
 
-// userRouter.post("/register", async (req:any, res:any) => {
-//     console.log("Entering register route");
-//     const {
-//         email,
-//         username,
-//         password
-//     } = req.body;
-
-//     if(!email || !username || ! password)
-//         return res.status(400).send({message: "Did not recieve all parameters"});
-
-//     console.log("email, username, password", email, username, password, "if only we had neo4j");
-
-//     const emailStatus = await redisClient.hExists(IdPrefixes.USER_EMAILS, email);
-//     console.log("Email unique check");
-//     if(emailStatus) //Check if email is already used
-//         return res.status(400).send({message: `User with email already exists`,
-//                                      email:email});
-    
-//     const fullUsername = `${username}_${nanoid()}`;
-//     console.log("username", fullUsername);
-//     //Erm what the sigma why is this not neo4j who could have forgotten to
-//     // chane it from REDIS ?????? I wonder....
-//     const usernameExists = 
-//     await redisClient.sIsMember(IdPrefixes.USERNAMES, fullUsername);
-//     console.log("Username exsists:",usernameExists);
-//     if(usernameExists)
-//         return res.status(500).send({message: "User with same full username exists"});
-
-//     try {
-//         // this is used for fast checking if username or email already exist
-//         console.log(await redisClient.hGetAll(IdPrefixes.USER_EMAILS));
-//         console.log("adding email");
-//         await redisClient.hSet(IdPrefixes.USER_EMAILS, email, fullUsername); //add email to all emails
-//         console.log("added email");
-//         await redisClient.sAdd(IdPrefixes.USERNAMES, fullUsername); // add username to usernames
-
-//         await redisClient.set(fullUsername, password);
-
-//         return res.status(200).send({message: "Added user succesfuly", fullUsername:fullUsername});
-//     }
-//     catch(err:any) {
-//         return res.status(400).send({message: "Failed to create user", reason:err.message});
-//     }
-    
 
 // });
 // //chat is this real we still don't have neo4j
